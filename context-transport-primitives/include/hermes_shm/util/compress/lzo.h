c53fa22e74e565143d613c5fe62f7bf9297b6741
rajnipawar <rajnippawar20@gmail.com>
2025-12-20 21:12:08 +0000

    fixed lzo and added a new benchmark with analysis script and plots

--- context-transport-primitives/include/hermes_shm/util/compress/lzo.h
+++ context-transport-primitives/include/hermes_shm/util/compress/lzo.h
@@ -22,13 +22,18 @@
 namespace hshm {
 
 class Lzo : public Compressor {
+ private:
+  // Work memory buffer required by LZO for compression
+  // LZO1X_1_15_MEM_COMPRESS is typically ~64-128KB depending on platform
+  alignas(16) unsigned char work_mem_[LZO1X_1_15_MEM_COMPRESS];
+
  public:
   bool Compress(void *output, size_t &output_size, void *input,
                 size_t input_size) override {
     int ret = lzo1x_1_15_compress(
         reinterpret_cast<const lzo_bytep>(input), input_size,
-        reinterpret_cast<lzo_bytep>(output), &output_size, nullptr);
-    return ret != 0;
+        reinterpret_cast<lzo_bytep>(output), &output_size, work_mem_);
+    return ret == 0;  // LZO returns 0 (LZO_E_OK) on success
   }
 
   bool Decompress(void *output, size_t &output_size, void *input,
@@ -36,7 +41,7 @@ class Lzo : public Compressor {
     int ret = lzo1x_decompress(reinterpret_cast<const lzo_bytep>(input),
                                input_size, reinterpret_cast<lzo_bytep>(output),
                                &output_size, nullptr);
-    return ret != 0;
+    return ret == 0;  // LZO returns 0 (LZO_E_OK) on success
   }
 };
 
