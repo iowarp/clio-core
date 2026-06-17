f49e299c02df45e83bf96dab825a70832144cfa2
yuvankas1 <yuvantrp@gmail.com>
2026-06-17 15:07:54 -0500

    fix(bdev): fix CI linker error by gating S3 transport instantiation

--- context-runtime/modules/bdev/src/transports/bdev_transport.cc
+++ context-runtime/modules/bdev/src/transports/bdev_transport.cc
@@ -6,7 +6,10 @@
 #include <clio_runtime/bdev/transports/bdev_transport.h>
 #include <clio_runtime/bdev/transports/fs_bdev_transport.h>
 #include <clio_runtime/bdev/transports/mem_bdev_transport.h>
+
+#ifdef CLIO_ENABLE_AMAZON_DRIVE
 #include <clio_runtime/bdev/transports/s3_bdev_transport.h>
+#endif
 
 namespace clio::run::bdev {
 
@@ -15,13 +18,13 @@ std::unique_ptr<BdevTransport> BdevTransportFactory::Create(BdevType type) {
     case BdevType::kFile:
       return std::make_unique<FsBdevTransport>();
     case BdevType::kRam:
-      return std::make_unique<MemBdevTransport>();
-    // Assume kPinned or kHbm are currently using RAM path based on original bdev_runtime.cc
     case BdevType::kHbm:
     case BdevType::kPinned:
       return std::make_unique<MemBdevTransport>(); 
+#ifdef CLIO_ENABLE_AMAZON_DRIVE
     case BdevType::kS3:
       return std::make_unique<S3BdevTransport>();
+#endif
     default:
       return nullptr;
   }
